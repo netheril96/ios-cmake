623cfdbf446ecd83bab0887149c19ba8ebd75e4b
Siyuan Ren <rsy@Siyuans-MacBook-Pro.local>
2017-05-26 14:22:00 +0800

    Remove the need for gcc

--- toolchain/iOS.cmake
+++ toolchain/iOS.cmake
@@ -45,12 +45,6 @@ if (CMAKE_UNAME)
 	string (REGEX REPLACE "^([0-9]+)\\.([0-9]+).*$" "\\1" DARWIN_MAJOR_VERSION "${CMAKE_HOST_SYSTEM_VERSION}")
 endif (CMAKE_UNAME)
 
-# Force the compilers to gcc for iOS
-include (CMakeForceCompiler)
-CMAKE_FORCE_C_COMPILER (/usr/bin/gcc Apple)
-CMAKE_FORCE_CXX_COMPILER (/usr/bin/g++ Apple)
-set(CMAKE_AR ar CACHE FILEPATH "" FORCE)
-
 # Skip the platform compiler checks for cross compiling
 set (CMAKE_CXX_COMPILER_WORKS TRUE)
 set (CMAKE_C_COMPILER_WORKS TRUE)
